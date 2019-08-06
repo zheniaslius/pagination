63059f068cabc1e2a55a6ab9bae7326c7f6ceb03
evsliusar <sluisarchuk@gmail.com>
2019-08-06 11:45:31 +0300

    styles complete

--- src/components/ItemsList/index.js
+++ src/components/ItemsList/index.js
@@ -5,8 +5,28 @@ import { loadNextPage, loadPrevPage } from '../../actions';
 
 import './ItemList.css';
 
-const ItemList = () => {
+const Controls = ({ users, page }) => {
   const dispatch = useDispatch();
+  return (
+    <div className="btn-wrapper">
+      <div>
+        <button
+          onClick={() => dispatch(loadPrevPage())}
+          disabled={!page}>
+          &#8249;
+        </button>
+        <span className="page-counter">{page+1}</span>
+        <button
+          onClick={() => dispatch(loadNextPage())}
+          disabled={page + 1 >= users.length / 5}>
+          &#8250;
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const ItemList = () => {
   const users = useSelector(state => state.users);
   const page = useSelector(state => state.page);
   const visibleUsers = users.slice(page * 5, (page * 5) + 5);
@@ -16,10 +36,10 @@ const ItemList = () => {
       <table className="items-table">
         <thead>
           <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>surname</td>
-            <td>description</td>
+            <th>id</th>
+            <th>name</th>
+            <th>surname</th>
+            <th>description</th>
           </tr>
         </thead>
         <tbody>
@@ -27,17 +47,8 @@ const ItemList = () => {
             <Item key={user.id} {...user}/>
           )}
         </tbody>
-        <button
-          onClick={() => dispatch(loadPrevPage())}
-          disabled={!page}>
-          prev
-        </button>
-        <button
-          onClick={() => dispatch(loadNextPage())}
-          disabled={page + 1 >= users.length / 5}>
-          next
-        </button>
       </table>
+      <Controls users={users} page={page}/>
     </div>
   );
 };
